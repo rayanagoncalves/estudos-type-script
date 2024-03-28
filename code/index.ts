6b9281b68a542cc13e9c8e639bb0ecfb4b4e05f3
rayanagoncalves <rayana.goncalves.cunha@hotmail.com>
2024-03-27 21:38:07 -0300

    Utilizando Tuple

--- code/index.ts
+++ code/index.ts
@@ -1,8 +1,11 @@
 let price = 40
-let item = "apple"
+//let item = "apple"
 let inStore = true
 
 let value // variavel do tipo ANY (ma pratica)
 value = 10
 
 let values: number[] = [10, 20, 30]
+
+// Apple, 3, true
+let item: [string, number, boolean] = ['apple', 3, true] // TUPLE
