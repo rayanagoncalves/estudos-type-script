66dbf0f77899785e27e5d1134cada5648c1c24bc
rayanagoncalves <rayana.goncalves.cunha@hotmail.com>
2024-03-29 16:04:09 -0300

    Utilizando Enum

--- code/index.ts
+++ code/index.ts
@@ -9,3 +9,14 @@ let values: number[] = [10, 20, 30]
 
 // Apple, 3, true
 let item: [string, number, boolean] = ['apple', 3, true] // TUPLE
+
+// ENUM
+enum Role {admin = 1, read = 2, backup = 3}
+
+const user = {
+    firstName: 'Rayana',
+    age: 24,
+    role: Role.admin
+}
+
+console.log(user)
--- prod/index.js
+++ prod/index.js
@@ -1,7 +1,22 @@
 "use strict";
 let price = 40;
-let item = "apple";
+//let item = "apple"
 let inStore = true;
 let value; // variavel do tipo ANY (ma pratica)
 value = 10;
 let values = [10, 20, 30];
+// Apple, 3, true
+let item = ['apple', 3, true]; // TUPLE
+// ENUM
+var Role;
+(function (Role) {
+    Role[Role["admin"] = 1] = "admin";
+    Role[Role["read"] = 2] = "read";
+    Role[Role["backup"] = 3] = "backup";
+})(Role || (Role = {}));
+const user = {
+    firstName: 'Rayana',
+    age: 24,
+    role: Role.admin
+};
+console.log(user);
