7bce9642b525db820aa0bc1515f228c84c07cab2
rayanagoncalves <rayana.goncalves.cunha@hotmail.com>
2024-04-01 20:39:57 -0300

    Exercício 5

--- prod/index.js
+++ prod/index.js
@@ -86,3 +86,32 @@ console.log(`I like to eat ${pie}-flavored pie`);
 // Day 3 - Exercise 3
 let isDouglas = true;
 console.log(`${isDouglas ? 'Oh, hi Douglas' : 'Who are you?'}`);
+// Day 3 - Exercise 4
+const integer = 6;
+const float = 6.66;
+const hex = 0xf00d;
+const binary = 0b1010011010;
+const octal = 0o744;
+const negZero = -0;
+const actuallyNumber = NaN;
+const largestNumber = Number.MAX_VALUE;
+const mostBiglyNumber = Infinity;
+const members = [
+    integer,
+    float,
+    hex,
+    binary,
+    octal,
+    negZero,
+    actuallyNumber,
+    largestNumber,
+    mostBiglyNumber
+];
+members[0] = 12345;
+console.log(members);
+// Day 3 - Exercise 5
+const sequence = Array.from(Array(10).keys());
+const animals = ['pangolin', 'aardvark', 'echidna', 'binturong'];
+const stringsAndNumbers = [1, 'one', 2, 'two', 3, 'three'];
+const allMyArrays = [sequence, animals, stringsAndNumbers];
+console.log(allMyArrays);
