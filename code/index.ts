cd3204273dd6b607c9f2d841df767db6cc56edea
rayanagoncalves <rayana.goncalves.cunha@hotmail.com>
2024-04-01 20:23:53 -0300

    Exercício 1

--- code/index.ts
+++ code/index.ts
@@ -119,3 +119,10 @@ const firstEmployee: Employee = {
     id: 12, 
     role: 'Admin'
 }
+
+// Day 3 - Exercise 1
+
+let pi = 3.14159;
+let tau = pi * 2;
+
+console.log(`${tau} is ${pi} times two.`);
