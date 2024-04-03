7720a1c1ebd84a261369e90cfa3fdbc01fbc5da8
rayanagoncalves <rayana.goncalves.cunha@hotmail.com>
2024-04-03 15:22:31 -0300

    Criando classe de cliente

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,20 @@
-console.log('Hi!')
+class Client {
+    name: string
+    private balance: number
+
+    constructor(name: string, balance: number) {
+        this.name = name
+        this.balance = balance
+    }
+
+    addMoney(amount: number) {
+        this.balance += amount
+    }
+}
+
+const firstClient = new Client("Rayana", 500)
+firstClient.addMoney(100)
+const secondClient = new Client("Maria", 200)
+
+console.log(firstClient)
+console.log(secondClient)
