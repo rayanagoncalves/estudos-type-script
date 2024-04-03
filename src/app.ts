958e1854e292fe9c1ced309fa33a26d22871b8c5
rayanagoncalves <rayana.goncalves.cunha@hotmail.com>
2024-04-03 15:29:04 -0300

    Criando interface de Produto

--- src/app.ts
+++ src/app.ts
@@ -18,3 +18,22 @@ const secondClient = new Client("Maria", 200)
 
 console.log(firstClient)
 console.log(secondClient)
+
+interface Item {
+    name: string, 
+    price: number
+
+    itemPurchased(message: string): void
+}
+
+let firstProduct: Item
+firstProduct = {
+    name: 'Apple',
+    price: 2,
+
+    itemPurchased(message: string): void {
+        console.log(message + ' ' + this.name)
+    }
+}
+
+firstProduct.itemPurchased('You just bought a')
